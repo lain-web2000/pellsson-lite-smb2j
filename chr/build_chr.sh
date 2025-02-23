083c107bd9fba9b20b88ea9561865e0d1933045f
Lain <51301652+lain-web2000@users.noreply.github.com>
2025-02-23 01:56:12 +0000

    Initial Commit

---
+++ chr/build_chr.sh
@@ -0,0 +1,7 @@
+set -e
+python genchr.py
+
+cat smb/lost-sprites.chr smb/lost-back.chr smb/lost-victory.chr lost-smborg-charset.chr \
+    smb/lost-sprites.chr smb/lost-back.chr smb/lost-victory.chr smb/lost-victory.chr smb/lost-victory.chr \
+	intro/intro-bg.chr intro-sprbank0.chr > full.chr
+
---
+++ common/ines.asm
@@ -0,0 +1,14 @@
+.segment "ines"
+.byte $4E,$45,$53,$1A ; INES header
+.byte $08             ; PRG pages
+.byte $4              ; CHR pages
+.byte $52             ; MMC5, battery-backed WRAM
+.byte $00             ; 
+.byte $00             ; 
+.byte $00             ; NTSC
+.byte $00             ; 
+.byte $00             ; 
+.byte $00             ; 
+.byte $00             ; 
+.byte $00             ; 
+.byte $00             ; 
